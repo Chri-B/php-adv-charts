6bb2ceea711f02cbae11253c23980e9931f95230
Christian Beretta <beretta.christian2602@gmail.com>
2020-04-22 16:08:05 +0200

    modifiche js

--- js/main.js
+++ js/main.js
@@ -4,25 +4,31 @@ $(document).ready(function() {
         url: 'server_line_chart.php',
         method: 'GET',
         success: function(data) {
-            var mesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
-            var lineChart = new Chart($('#line-chart'), {
-                type: 'line',
-                data: {
-                    labels: mesi,
-                    datasets: [{
-                        label: 'Vendite Mensili Anno 2020',
-                        backgroundColor: 'rgba(255, 0, 0, 0.3)',
-                        borderColor: 'rgb(255, 0, 0)',
-                        data: data,
-                        lineTension: 0
-                    }]
-                }
-            });
+            var arrayMesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+            createLineChart(arrayMesi, data);
         },
         error: function (err) {
             alert('Errore comunicazione');
         }
     });
+
+
+    // funzione per creazione grafico Line - variabili in ingresso: labels (mesi) e dati
+    function createLineChart(arrayMesi, data) {
+        var lineChart = new Chart($('#line-chart'), {
+            type: 'line',
+            data: {
+                labels: arrayMesi,
+                datasets: [{
+                    label: 'Vendite Mensili Anno 2020',
+                    backgroundColor: 'rgba(255, 0, 0, 0.3)',
+                    borderColor: 'rgb(255, 0, 0)',
+                    data: data,
+                    lineTension: 0
+                }]
+            }
+        });
+    }
 
 
 });
